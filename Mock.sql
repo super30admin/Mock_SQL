e5b1a4eb7826f189e185a1568293ecc3d6f3032b
Akshathapatil1998 <71171604+Akshathapatil1998@users.noreply.github.com>
2023-09-04 18:33:14 -0400

    Solved week 2 Mock Interview questions

--- Mock.sql
+++ Mock.sql
@@ -13,3 +13,13 @@ order by travelled_distance desc,name asc
 Select s1.sale_date, s1.sold_num-s2.sold_num as diff from Sales s1
 join Sales s2 on s1.sale_date=s2.sale_date 
 where s1.fruit IN('apples') and s2.fruit IN('oranges')
+
+-- Customers who brought all products
+select customer_id from Customer group by customer_id having 
+count(distinct product_key)=(Select count(product_key) as cnt from Product)
+
+
+-- Product Sales Analysis 3
+select product_id,Sales.year as first_year, quantity, price from Sales
+where (product_id,Sales.year) in( select product_id,min(year) as first_year  
+from Sales group by product_id)
