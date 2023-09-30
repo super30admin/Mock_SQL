3eed9470361a968c72a2a340720bc75d1bf43db5
Krishna Dheeraj Krovi <krishnadheeraj22@gmail.com>
2023-09-29 19:35:10 -0700

    Modified Week 3 SQL

--- Week-3/Problem1.sql
+++ Week-3/Problem1.sql
@@ -1,22 +1,28 @@
 /*
-Tournament Winners
-1. Compute the match statistics in first player perspective to compute the scores they won as p1
-2. Compute the match statistics in second player's perspective to compute scores they won as p1
-3. Combine the stats to find which player scored most overall by ranking on the sum of their scores in descending
+1. Find the items that were sold 2nd ranked by date for each seller
+2. Also find the brand and seller from Items table for above result
+3. From the users who have their favorite brand as that sold by them 2nd based on date
 */
 
 with cte as
-(select first_player 'p1',second_player 'p2',first_score 's1',second_score 's2', group_id
-from matches m1 join players p 
-on m1.first_player=p.player_id
-union all
-select second_player 'p1',first_player 'p2', second_score 's1', first_score 's2', group_id
-from matches m2 join players p 
-on m2.first_player=p.player_id
+(
+  select item_id,seller_id from
+  (
+  select  item_id, seller_id, rank() over (partition by seller_id order by order_date) 'rnk'
+  from
+  Orders
+  ) a
+  where rnk = 2
 ),
-cte2 as 
-(select p1,group_id, dense_rank() over (partition by group_id order by sum(s1) desc,p1) 'rnk' from cte
-group by p1)
-select group_id, p1 'player_id'
-from cte2 
-where rnk = 1
+cte2 as
+(
+select i.item_brand, cte.seller_id from
+cte join Items i
+on cte.item_id = i.item_id
+)
+select u.user_id 'seller_id', if(cte2.item_brand= u.favorite_brand, 'yes','no') '2nd_item_fav_brand'
+from
+Users u
+left join
+cte2
+on u.user_id = cte2.seller_id
